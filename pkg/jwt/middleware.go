da05f18588f8f0abce133b4329e37acd09b33681
GaoShuai <gaoshuai@tuyougame.com>
2023-11-16 01:55:56 +0800

    feat: token 判断 Bearer

--- pkg/jwt/middleware.go
+++ pkg/jwt/middleware.go
@@ -2,13 +2,14 @@ package jwt
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4/request"
 	"net/http"
 )
 
 // JWT is jwt middleware
 func JWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.GetHeader(AUTHORIZATION)
+		token, err := request.AuthorizationHeaderExtractor.ExtractToken(c.Request)
 		if token == "" {
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
